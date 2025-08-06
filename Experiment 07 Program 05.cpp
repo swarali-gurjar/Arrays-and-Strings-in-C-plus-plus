cbacf724beffc0b701e44ebb22bd16aae3390521
Swarali Gurjar <gurjar.swarali@gmail.com>
2025-08-05 23:44:52 -0400

    Experiment 07 Program 05.cpp

---
+++ Experiment 07 Program 05.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    string input;
+    cout << "Enter a string: ";
+    cin >> input;
+    cout << "You entered: " << input << endl;
+    return 0;
+}
